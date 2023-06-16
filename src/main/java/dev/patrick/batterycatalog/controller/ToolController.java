84a06b45a2e8c19694e96c9609bcae8c5e00b70d
Patrick-SGomes <patrick.gomesrb@gmail.com>
2023-06-15 21:32:01 -0300

    Iniciando CRUD das chaves

---
+++ src/main/java/dev/patrick/batterycatalog/controller/ToolController.java
@@ -0,0 +1,5 @@
+package dev.patrick.batterycatalog.controller;
+
+public class ToolController {
+
+}
---
+++ src/main/java/dev/patrick/batterycatalog/model/ToolModel.java
@@ -0,0 +1,44 @@
+package dev.patrick.batterycatalog.model;
+
+public class ToolModel {
+    private int id;
+    private String nome;
+    private String tamanho;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String toString() {
+        return nome;
+    }
+}
